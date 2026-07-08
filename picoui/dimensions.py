9b632055efa89464911153fa15f59c4940f51519
Brooks, Mark <mark.brooks@evotec.com>
2026-07-08 09:57:12 -0400

    optimizing arrow and ribbon

--- picoui/dimensions.py
+++ picoui/dimensions.py
@@ -8,22 +8,24 @@ from PySide6.QtCore import QSize
 
 @dataclass
 class Dimensions:
-    """Dimensions"""
-    width = 100
-    height = 100
-    min_height = 100
-    max_height = 100
-    min_width = 100
-    max_width = 100
-    radius = 10
-    point_size = 5
-    line_width = 1.2
-    size = (width, height)
+    """Dimensions of the UI (width, height, and radius, radius, point_size"""
+    width: int = 100
+    height: int = 100
+    min_height: int = 100
+    max_height: int = 100
+    min_width: int = 100
+    max_width: int = 100
+    radius: int = 10
+    point_size: int = 5
+    line_width: float = 1.2
     spacing: int = 10
 
-    @classmethod
-    def to_tuple(cls):
-        return cls.width, cls.height
+    @property
+    def size(self) -> tuple[int, int]:
+        return self.width, self.height
+
+    def to_tuple(self) -> tuple[int, int]:
+        return self.width, self.height
 
 
 class PicoUiDimensions:
